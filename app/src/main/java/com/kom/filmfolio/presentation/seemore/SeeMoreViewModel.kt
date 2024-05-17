45c3afae57707c5a8bb33c978871cbd0b7d93e63
bellafebrianyn <bellafebrianynws@gmail.com>
2024-05-17 16:00:45 +0700

    Finishing feature see more movies

--- app/src/main/java/com/kom/filmfolio/presentation/seemore/SeeMoreViewModel.kt
+++ app/src/main/java/com/kom/filmfolio/presentation/seemore/SeeMoreViewModel.kt
@@ -1,7 +1,6 @@
 package com.kom.filmfolio.presentation.seemore
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
@@ -12,7 +11,6 @@ import com.kom.filmfolio.paging.NowPlayingMoviePagingSource
 import com.kom.filmfolio.paging.PopularMoviePagingSource
 import com.kom.filmfolio.paging.TopRelatedMoviePagingSource
 import com.kom.filmfolio.paging.UpcomingMoviePagingSource
-import kotlinx.coroutines.Dispatchers
 
 class SeeMoreViewModel(
     private val movieRepository: MovieRepository,
@@ -49,24 +47,4 @@ class SeeMoreViewModel(
             UpcomingMoviePagingSource(filmfolioApiService)
         }.flow
             .cachedIn(viewModelScope)
-
-    fun getNowPlayingMovie(
-        language: String,
-        page: Int,
-    ) = movieRepository.getNowPlayingMovie(language, page).asLiveData(Dispatchers.IO)
-
-    fun getPopularMovie(
-        language: String,
-        page: Int,
-    ) = movieRepository.getPopularMovie(language, page).asLiveData(Dispatchers.IO)
-
-    fun getTopRelatedMovie(
-        language: String,
-        page: Int,
-    ) = movieRepository.getTopRelatedMovie(language, page).asLiveData(Dispatchers.IO)
-
-    fun getUpcomingMovie(
-        language: String,
-        page: Int,
-    ) = movieRepository.getUpcomingMovie(language, page).asLiveData(Dispatchers.IO)
 }
